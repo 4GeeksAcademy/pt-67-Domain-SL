7b5d932eb2a42a747f8c65b3186d7c67aa5ff579
Sonia <161872642+Inflanubes@users.noreply.github.com>
2024-05-08 20:58:53 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "it's a"];
+  let adj = ["great", "big", "stupid", "bad"];
+  let noun = ["404", "domain", "website", "idea"];
+  // añadir un if termina en es, se escribe .es / else añadir .com
+  let domains = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        console.log(pronoun[i] + adj[j] + noun[k]);
+        const domain = pronoun[i] + adj[j] + noun[k] + ".com";
+        document.getElementById("domain").innerHTML = domain;
+        domains.push(domain);
+      }
+    }
+  }
+  console.log(domains);
 };
